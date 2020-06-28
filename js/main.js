aa2717be4ab98f0098ca739e767ae08fbdabc559
Pavel Hancharou <pl.hancharou@gmail.com>
2020-06-28 20:16:17 +0300

    Upload new files + adaptive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 var mySwiper = new Swiper('.swiper-container', {
-  slidesPerView: 2,
+  slidesPerView: 1,
   loop: true,
   navigation: {
     nextEl: ".arrow"
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2
+    }
   }
+});
+
+var mBtn = document.querySelector('.menu-button');
+var menu = document.querySelector('.header');
+mBtn.addEventListener('click', function () {
+  mBtn.classList.toggle('menu-button-active');
+  menu.classList.toggle('header-active');
 });
